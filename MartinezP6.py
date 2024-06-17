c58eef28c471af55627e4f52f38a96fcf5fa9b2d
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-17 10:12:29 -0600

    Testing program

--- MartinezP6.py
+++ MartinezP6.py
@@ -30,3 +30,42 @@ def header():
     lon1 = radians(lon1)
     lat1 = radians(lat2)
     lon2 = radians(lon2)
+
+    # Given formula 
+    a = sin((lat2 - lat1) / 2)**2 + cos(lat1) * cos(lat2) * sin((lon2 - lon1) / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = R * c
+
+    return distance
+  
+# Main
+def main():
+  # Call the header function
+  header()
+
+  while true: 
+    # Get the first location 
+    print("\nEnter the first location: ")
+    point1 = get_location()
+
+    # Get second location
+    print("\nEnter the second location: ")
+    point2 = get_location()
+
+    # Calculate the distance between 2 points
+    dist = distance(point1, point2)
+
+    # Display results 
+    print(f"\nThe distance between the two points is {dist:.2f} kilometers.")
+
+    # Ask the user if they want to calculate another distance, break if no
+    another = input("\nDo you want to calculate another distance? (yes/no): ")
+    if another != 'yes':
+      break
+
+# Goodbye message and thanks
+print("\nThank you for using this program, Goodbye!")
+
+# Run main function 
+if _name_ == "_main_":
+  main()
